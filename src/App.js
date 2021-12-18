868657e86e897c9a34055eff9b11fad03ec97c73
ShammiAktari <87249241+ShammiAktari@users.noreply.github.com>
2021-12-18 14:59:48 +0000

    import react weather

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='App-weather'>
+          <form>
+            <input className="search" type="search" placeholder='enter your city' />
+            <input type="submit" value="Search" />
+          </form>
+          <Weather />
+        </div>
+        
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Weather (){
+    return(
+            <div className="Weather-app">
+        <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <h1>Lisbon</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <h1>Lisbon</h1>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
