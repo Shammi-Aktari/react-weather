0e13c0a54243e6421c0340e585162c48ff519170
ShammiAktari <87249241+ShammiAktari@users.noreply.github.com>
2021-12-18 15:51:22 +0000

    import react weather animation, react loader, created new component Weather.js and new css

--- src/App.js
+++ src/App.js
@@ -6,13 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='App-weather'>
-          <form>
-            <input className="search" type="search" placeholder='enter your city' />
-            <input type="submit" value="Search" />
-          </form>
+        
           <Weather />
-        </div>
+        
         
       </header>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+import ReactAnimatedWeather from 'react-animated-weather';
+import "./Weather.css";
 
-export default function Weather (){
-    return(
-            <div className="Weather-app">
-        <div className="container">
-                <div className="row">
-                    <div className="col-6">
-                        <h1>Lisbon</h1>
-                    </div>
-                </div>
-            </div>
+export default function Weather(props) {
+  let [city, setCity] = useState("");
+  let [message, setMessage] = useState("");
 
-            <div className="container">
-                <div className="row">
-                    <div className="col-6">
-                        <h1>Lisbon</h1>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+  function getForecast(response) {
+    console.log(response.data);
+    setMessage(
+      <ul>
+        <h2 className="city">{city}</h2>
+        <li>Temperature: {Math.round(response.data.main.temp)}ºC</li>
+        <li>Description: {response.data.weather[0].description}</li>
+        <li>Humidity: {response.data.main.humidity}%</li>
+        <li>Wind: {Math.round(response.data.wind.speed)} km/h</li>
+        <li>
+        
+          <img
+            src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+            alt={response.data.weather[0].description}
+          />
+        </li>
+      </ul>
+      
     );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "4ef1e54a9a52c90543ac2a54b2d5a60b";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(getForecast);
+  }
+
+  function getTemperature(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="Weather-app">
+        <ReactAnimatedWeather 
+        icon= 'CLEAR_DAY'
+        color= 'goldenrod'
+        animate= {true}
+        />
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input className="search"
+          type="search"
+          placeholder="enter your city"
+          onChange={getTemperature}
+        />
+        <input className="search-button" type="submit" value="Search" />
+      </form>
+      <br />
+      <Loader
+        type="Grid"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+      <p>{message}</p>
+    </div>
+  );
 }
